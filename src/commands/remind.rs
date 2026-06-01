e2e7d58b0064c695c72d2ad27a571465ae046b2a
hisuic <henryharlay@gmail.com>
2026-06-01 16:35:03 +0900

    add todo comment

--- src/commands/remind.rs
+++ src/commands/remind.rs
@@ -30,6 +30,8 @@ pub async fn run(ctx: &Context, msg: &Message, reminder_text: String) {
     // Debug: to check if the message is correctly loaded
     // println!("{}", reminder_text);
 
+// TODO: 12時間までは1時間おきにボタンを追加してもいいかも
+
     let show_buttons = msg
         .channel_id
         .send_message(
